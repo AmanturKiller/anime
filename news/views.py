c16a56c190c6842e773a8d9aff7019da5fa7a402
Amankaium <kaium@list.ru>
2024-07-15 12:49:52 +0600

    add news views count

--- news/views.py
+++ news/views.py
@@ -9,5 +9,13 @@ def news_list(request):
 def new_detail(request, id): # id = 8
     # print(id) # 8
     one_new_object = New.objects.get(id=id)  # 1 объект
+    
+    one_new_object.views += 1  # меняем значение свойства объекта
+    
+    if request.user.is_authenticated:
+        one_new_object.user_views.add(request.user)
+    
+    one_new_object.save()  # сохраняем в БД
+    
     context = {"new": one_new_object} 
     return render(request, 'new.html', context)
