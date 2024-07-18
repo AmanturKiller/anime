d2da77df3e123109ce2954f9eb4da9d7fadb8408
Amankaium <kaium@list.ru>
2024-07-18 12:53:58 +0600

    add bootstrap

--- core/static/code.js
+++ core/static/code.js
@@ -1,14 +1,14 @@
 // меням стили через JS
-const navLink = document.querySelector("nav a")
+// const navLink = document.querySelector("nav a")
 
-function changeStyle(e) {
-    navLink.style.backgroundColor = 'darkblue'
-}
+// function changeStyle(e) {
+//     navLink.style.backgroundColor = 'darkblue'
+// }
 
-navLink.addEventListener("mouseover", changeStyle)
+// navLink.addEventListener("mouseover", changeStyle)
 
 
 // меняем html через JS
-const nav = document.getElementsByTagName("nav")[0]
-nav.innerHTML += '<a href="/baskets/">Корзины</a>'
+// const nav = document.getElementsByTagName("nav")[0]
+// nav.innerHTML += '<a href="/baskets/">Корзины</a>'
 
